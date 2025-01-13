a0fdba658e149a6a4a4a7dfe88751d64238fce04
Keilizon-Deiv D. Leones <kede.leones.up@phinmaed.com>
2025-01-13 15:52:33 +0800

    Update main.dart

---
+++ herbaplant/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'frontend/user/Screen/Homeuser.dart';
+import 'frontend/Auth/UserSignin.dart';
+import 'frontend/user/Screen/main_navigation.dart'; // Import MainNavigation
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Herbal App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      // Start with UserSignin screen
+      home: UserSignin(),
+    );
+  }
+}
